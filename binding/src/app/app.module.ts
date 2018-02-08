1a485da817c092058e7e8db8ff7b9db1363e3a5e
Daniel_Marcello <daniel.salinas.tellez@gmail.com>
2018-02-08 07:35:52 -0800

    two way bindings [()]

--- binding/src/app/app.module.ts
+++ binding/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { Test2Component } from './test2/test2.component';
 import { EventBindingComponent } from './event-binding/event-binding.component';
+import { TemplateRefVarsComponent } from './template-ref-vars/template-ref-vars.component';
+import { TwoWayBindingComponent } from './two-way-binding/two-way-binding.component';
 
 
 @NgModule({
@@ -13,10 +16,13 @@ import { EventBindingComponent } from './event-binding/event-binding.component';
     AppComponent,
     TestComponent,
     Test2Component,
-    EventBindingComponent
+    EventBindingComponent,
+    TemplateRefVarsComponent,
+    TwoWayBindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ binding/src/app/template-ref-vars/template-ref-vars.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-ref-vars',
+  templateUrl: './template-ref-vars.component.html',
+  styleUrls: ['./template-ref-vars.component.css']
+})
+export class TemplateRefVarsComponent implements OnInit {
+
+  public name = "Daniel";
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  logMessage(value){
+  	console.log(value)
+  }
+
+}
