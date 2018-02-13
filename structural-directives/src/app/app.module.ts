17eb47a4ea8986f64d7f8f50f26e5d000983eafd
Daniel_Marcello <daniel.salinas.tellez@gmail.com>
2018-02-13 07:04:30 -0800

    ngfor

--- structural-directives/src/app/app.module.ts
+++ structural-directives/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
 import { TestSwitchComponent } from './test-switch/test-switch.component';
+import { TestForComponent } from './test-for/test-for.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
-    TestSwitchComponent
+    TestSwitchComponent,
+    TestForComponent
   ],
   imports: [
     BrowserModule
